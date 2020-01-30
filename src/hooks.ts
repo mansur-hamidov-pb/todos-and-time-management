985318f59a11704ecce8ca9451b51f1a645ff0ff
Mansur Hamidov <mhamidov@Mansur-Hamidov.local>
2020-01-30 10:35:57 +0400

    Optimized useTodos hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
+import * as todos from './modules/Todos/actions';
 import { TodoContext } from './modules/Todos/components/TodoProvider';
+import { ITodo } from './modules/Todos/models';
 
 export function useTodos () {
-    return useContext(TodoContext)
+    const { list, dispatch } = useContext(TodoContext);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(list));
+    }, [list]);
+
+    return {
+        list,
+        addTodo: (payload: Partial<ITodo>) => dispatch(todos.addTodo(payload)),
+        removeTodo: (id: ITodo['id']) => dispatch(todos.removeTodo(id)),
+        startTimer: (id: ITodo['id']) => dispatch(todos.startTimer(id)),
+        pauseTimer: (id: ITodo['id']) => dispatch(todos.pauseTimer(id)),
+        stopTimer: (id: ITodo['id']) => dispatch(todos.stopTimer(id)),
+        toggleDone: (id: ITodo['id']) => dispatch(todos.toggleDone(id))
+    }
 }
--- src/modules/Todos/components/TodoProvider.tsx
+++ src/modules/Todos/components/TodoProvider.tsx
@@ -5,7 +5,7 @@ import { ITodo } from '../models';
 import { todosReducer, initialState } from '../reducer';
 
 interface IContext {
-    todos: ITodo[];
+    list: ITodo[];
     dispatch: (action: any) => void;
 }
 
@@ -13,14 +13,10 @@ export const TodoContext = React.createContext<IContext>({} as IContext);
 
 export const TodoProvider: React.FC = ({ children }) => {
     const initialTodos = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[];
-    const [todos, dispatchTodoAction] = React.useReducer(todosReducer, initialTodos || initialState);
-
-    React.useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos])
+    const [list, dispatchTodoAction] = React.useReducer(todosReducer, initialTodos || initialState);
 
     return (
-        <TodoContext.Provider value={{todos, dispatch: dispatchTodoAction}}>
+        <TodoContext.Provider value={{list, dispatch: dispatchTodoAction}}>
             {children}
         </TodoContext.Provider>
     )
